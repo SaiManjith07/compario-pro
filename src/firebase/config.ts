deecd2d020f50d686d0028c9130f486b3f3e8e3d
Saimanjith Paripelli <paripellisaimanjith@gmail.com>
2025-12-05 18:20:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4533083727-48f66",
+  "appId": "1:831158211162:web:889fd8191e5167debb3e63",
+  "apiKey": "AIzaSyDEw2u236htBpN6iAl9XiEJDH70I7ERpr0",
+  "authDomain": "studio-4533083727-48f66.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "831158211162"
+};
